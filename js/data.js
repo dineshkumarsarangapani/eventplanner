cd4cb215fe0fe1591f772d37bff339e44f555b9a
dineshkumarinchennai@gmail.com <dineshkumarinchennai@gmail.com>
2014-07-12 22:54:07 +0530

    Index Html

---
+++ js/data.js
@@ -0,0 +1,35 @@
+var data = {
+		'Marriagehall' :[
+		        {'id':1,'rate' : 10000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor3.png'},
+		        {'id':2,'rate' : 100000, 'rating':4.5, 'imgpath': 'images/Marriagehall/Vendor7.png'},
+		        {'id':3,'rate' : 200000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor8.png'},
+		        {'id':4,'rate' : 750000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor6.png'},
+		        {'id':5,'rate' : 500000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor1.png'},
+		        {'id':6,'rate' : 1000000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor4.png'},
+		        {'id':7,'rate' : 300000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor9.png'},
+		        {'id':8,'rate' : 900000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor5.png'},
+		        {'id':9,'rate' : 150000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor2.png'}
+		       ],
+		'Food':[
+		        {'id':10,'rate' : 500, 'rating':3, 'imgpath': 'images/Food/Food1.png'},
+		        {'id':11,'rate' : 1000, 'rating':3, 'imgpath': 'images/Food/Food2.png'},
+		        {'id':12,'rate' : 200, 'rating':3, 'imgpath': 'images/Food/Food3.png'},
+		        {'id':13,'rate' : 750, 'rating':3, 'imgpath': 'images/Food/Food4.png'},
+		        {'id':14,'rate' : 2000, 'rating':3, 'imgpath': 'images/Food/Food5.png'},
+		        {'id':15,'rate' : 2300, 'rating':3, 'imgpath': 'images/Food/Food8.png'},
+		        {'id':16,'rate' : 1500, 'rating':3, 'imgpath': 'images/Food/Food1.png'},
+		        {'id':17,'rate' : 200, 'rating':3, 'imgpath': 'images/Food/Food7.png'},
+		        {'id':18,'rate' : 150, 'rating':3, 'imgpath': 'images/Food/Food6.png'}
+		        ],
+		'Photographer' :[
+				{'id':19,'rate' : 50000, 'rating':3, 'imgpath': 'images/Photographer/Photographer1.png'},
+				{'id':20,'rate' : 75000, 'rating':3, 'imgpath': 'images/Photographer/Photographer2.png'},
+				{'id':21,'rate' : 20000, 'rating':3, 'imgpath': 'images/Photographer/Photographer3.png'},
+				{'id':22,'rate' : 100000, 'rating':3, 'imgpath': 'images/Photographer/Photographer4.png'},
+				{'id':23,'rate' : 5000, 'rating':3, 'imgpath': 'images/Photographer/Photographer5.png'},
+				{'id':24,'rate' : 4000, 'rating':3, 'imgpath': 'images/Photographer/Photographer6.png'},
+				{'id':25,'rate' : 40000, 'rating':3, 'imgpath': 'images/Photographer/Photographer7.png'},
+				{'id':26,'rate' : 85000, 'rating':3, 'imgpath': 'images/Photographer/Photographer8.png'},
+				{'id':27,'rate' : 90000, 'rating':3, 'imgpath': 'images/Photographer/Photographer9.png'}
+		       ]
+}
