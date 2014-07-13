777eacb73960f10a73fae1ada89b205b9c7e2b04
Dan <danabalr@gmail.com>
2014-07-13 13:12:47 +0530

    updated

--- js/data.js
+++ js/data.js
@@ -1,36 +1,36 @@
 var Datadump = {
 		'Marriagehall' :[
-		        {'id':1,'rate' : 10000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor3.png' , 'minBudget' : 10000, 'name' :'aa'},
-		        {'id':2,'rate' : 100000, 'rating':4.5, 'imgpath': 'images/Marriagehall/Vendor7.png', 'minBudget' : 500000, 'name' :'aa'},
-		        {'id':3,'rate' : 200000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor8.png', 'minBudget' : 700000, 'name' :'aa'},
-		        {'id':4,'rate' : 750000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor6.png', 'minBudget' : 1000000, 'name' :'aa'},
-		        {'id':5,'rate' : 500000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor1.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':6,'rate' : 1000000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor4.png', 'minBudget' : 1000000, 'name' :'aa'},
-		        {'id':7,'rate' : 300000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor9.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':8,'rate' : 900000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor5.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':9,'rate' : 150000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor2.png', 'minBudget' : 100000, 'name' :'aa'}
+		        {'id':1,'rate' : 450000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor3.png' , 'minBudget' : 1, 'name' :'Rose Mahal', 'seat' : 500,'Rooms' : 4 },
+		        {'id':2,'rate' : 350000, 'rating':5, 'imgpath': 'images/Marriagehall/Vendor7.png', 'minBudget' : 1, 'name' :'Gandhi palace', 'seat' : 600,'Rooms' : 6 },
+		        {'id':3,'rate' : 300000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor8.png', 'minBudget' : 1, 'name' :'Rani Hall', 'seat' : 500,'Rooms' : 6 },
+		        {'id':4,'rate' : 450000, 'rating':4, 'imgpath': 'images/Marriagehall/Vendor6.png', 'minBudget' : 1, 'name' :'Nethaji Mahal','seat' : 500,'Rooms' : 4 },
+		        {'id':5,'rate' : 2500000, 'rating':5, 'imgpath': 'images/Marriagehall/Vendor1.png', 'minBudget' : 1, 'name' :'Hitech Mahal','seat' : 2000,'Rooms' : 14 },
+		        {'id':6,'rate' : 1200000, 'rating':4, 'imgpath': 'images/Marriagehall/Vendor4.png', 'minBudget' : 1, 'name' :'Super mahal','seat' : 1200,'Rooms' : 10 },
+		        {'id':7,'rate' : 200000, 'rating':2, 'imgpath': 'images/Marriagehall/Vendor9.png', 'minBudget' : 1, 'name' :'Vijay Mahal','seat' : 1400,'Rooms' : 12 },
+		        {'id':8,'rate' : 1000000, 'rating':5, 'imgpath': 'images/Marriagehall/Vendor5.png', 'minBudget' : 1, 'name' :'Raja Mahal','seat' : 1000,'Rooms' : 10 },
+		        {'id':9,'rate' : 500000, 'rating':3, 'imgpath': 'images/Marriagehall/Vendor2.png', 'minBudget' : 1, 'name' :'Giant Mahll', 'seat' : 600,'Rooms' : 5 } 
 		       ],
 		'Food':[
-		        {'id':10,'rate' : 500, 'rating':3, 'imgpath': 'images/Food/Food1.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':11,'rate' : 1000, 'rating':3, 'imgpath': 'images/Food/Food2.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':12,'rate' : 200, 'rating':3, 'imgpath': 'images/Food/food3.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':13,'rate' : 750, 'rating':3, 'imgpath': 'images/Food/food4.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':14,'rate' : 2000, 'rating':3, 'imgpath': 'images/Food/Food5.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':15,'rate' : 2300, 'rating':3, 'imgpath': 'images/Food/food8.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':16,'rate' : 1500, 'rating':3, 'imgpath': 'images/Food/food9.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':17,'rate' : 200, 'rating':3, 'imgpath': 'images/Food/food7.png', 'minBudget' : 100000, 'name' :'aa'},
-		        {'id':18,'rate' : 150, 'rating':3, 'imgpath': 'images/Food/food6.png', 'minBudget' : 100000, 'name' :'aa'}
+		        {'id':10,'rate' : 50, 'rating':3, 'imgpath': 'images/Food/Food1.png', 'minBudget' : 1, 'name' :'Ram cater', 'type' : 'Veg', 'items' : 4, 'buffet': 'No'},
+		        {'id':11,'rate' : 250, 'rating':4, 'imgpath': 'images/Food/Food2.png', 'minBudget' : 1, 'name' :'Super foods', 'type' : 'Veg', 'items' : 6, 'buffet': 'No'},
+		        {'id':12,'rate' : 200, 'rating':5, 'imgpath': 'images/Food/food3.png', 'minBudget' : 1, 'name' :'Saravanabhavan', 'type' : 'Non Veg', 'items' : 7, 'buffet': 'No'},
+		        {'id':13,'rate' : 250, 'rating':4, 'imgpath': 'images/Food/food4.png', 'minBudget' : 1, 'name' :'Sangeetha', 'type' : 'Veg', 'items' : 5, 'buffet': 'No'},
+		        {'id':14,'rate' : 300, 'rating':3, 'imgpath': 'images/Food/Food5.png', 'minBudget' : 1, 'name' :'easy foods' ,'type' : 'Veg', 'items' : 10, 'buffet': 'Yes'},
+		        {'id':15,'rate' : 1250, 'rating':5, 'imgpath': 'images/Food/food8.png', 'minBudget' : 1, 'name' :'Devine foods' , 'type' : 'Veg', 'items' : 18, 'buffet': 'Yes'},
+		        {'id':16,'rate' : 1500, 'rating':4, 'imgpath': 'images/Food/food9.png', 'minBudget' : 1, 'name' :'ABC foods', 'type' : 'Non Veg', 'items' : 12, 'buffet': 'No'},
+		        {'id':17,'rate' : 200, 'rating':4, 'imgpath': 'images/Food/food7.png', 'minBudget' : 1, 'name' :'Good foods', 'type' : 'Veg', 'items' : 6, 'buffet': 'No'},
+		        {'id':18,'rate' : 150, 'rating':5, 'imgpath': 'images/Food/food6.png', 'minBudget' : 1, 'name' :'Fast foods', 'type' : 'Veg', 'items' : 8, 'buffet': 'No'}
 		        ],
 		'Photographer' :[
-				{'id':19,'rate' : 50000, 'rating':3, 'imgpath': 'images/Photographer/Photographer1.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':20,'rate' : 75000, 'rating':3, 'imgpath': 'images/Photographer/Photographer2.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':21,'rate' : 20000, 'rating':3, 'imgpath': 'images/Photographer/Photographer3.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':22,'rate' : 100000, 'rating':3, 'imgpath': 'images/Photographer/Photographer4.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':23,'rate' : 5000, 'rating':3, 'imgpath': 'images/Photographer/Photographer5.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':24,'rate' : 4000, 'rating':3, 'imgpath': 'images/Photographer/Photographer6.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':25,'rate' : 40000, 'rating':3, 'imgpath': 'images/Photographer/Photographer7.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':26,'rate' : 85000, 'rating':3, 'imgpath': 'images/Photographer/Photographer8.png', 'minBudget' : 100000, 'name' :'aa'},
-				{'id':27,'rate' : 90000, 'rating':3, 'imgpath': 'images/Photographer/Photographer9.png', 'minBudget' : 100000, 'name' :'aa'}
+				{'id':19,'rate' : 50000, 'rating':5, 'imgpath': 'images/Photographer/Photographer1.png', 'minBudget' : 1, 'name' :'Super Clicks','HD Video': 'No'},
+				{'id':20,'rate' : 75000, 'rating':3, 'imgpath': 'images/Photographer/Photographer2.png', 'minBudget' : 1, 'name' :'Raja photos','HD Video': 'Yes'},
+				{'id':21,'rate' : 20000, 'rating':4, 'imgpath': 'images/Photographer/Photographer3.png', 'minBudget' : 1, 'name' :'lights on','HD Video': 'Yes'},
+				{'id':22,'rate' : 100000, 'rating':5, 'imgpath': 'images/Photographer/Photographer4.png', 'minBudget' : 1, 'name' :'ABC photo','HD Video': 'Yes'},
+				{'id':23,'rate' : 5000, 'rating':3, 'imgpath': 'images/Photographer/Photographer5.png', 'minBudget' : 1, 'name' :'Lense craft','HD Video': 'No'},
+				{'id':24,'rate' : 4000, 'rating':4, 'imgpath': 'images/Photographer/Photographer6.png', 'minBudget' : 1, 'name' :'Memories ','HD Video': 'No'},
+				{'id':25,'rate' : 40000, 'rating':3, 'imgpath': 'images/Photographer/Photographer7.png', 'minBudget' : 1, 'name' :'You memories','HD Video': 'No'},
+				{'id':26,'rate' : 85000, 'rating':5, 'imgpath': 'images/Photographer/Photographer8.png', 'minBudget' : 1, 'name' :'Ages photos','HD Video': 'Yes'},
+				{'id':27,'rate' : 90000, 'rating':3, 'imgpath': 'images/Photographer/Photographer9.png', 'minBudget' : 1, 'name' :'Bees photos','HD Video': 'Yes'}
 		       ]
 }
 
